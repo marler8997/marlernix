313a44d475e0dc43720bfc45ad74aac3a02c8e69
Jonathan Marler <johnnymarler@gmail.com>
2021-10-30 09:36:16 -0600

    update configuration for 21.05

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,7 +1,34 @@
 { config, lib, pkgs, ... }:
 
 {
+  # for some reason on nixos 21.05, this breaks emacs fonts
+  #fonts.fonts = with pkgs; [
+  #  fira-code
+  #];
+
+  #fonts.fonts = with pkgs; [
+  #  jetbrains-mono
+  #  dejavu_fonts
+  #  noto-fonts
+  #  hack-font
+  #];
   fonts.fonts = with pkgs; [
-    fira-code
+    # This one line fixed my wide terminal fonts and emacs font!!!!
+    iosevka
+    #noto-fonts
+    #noto-fonts-cjk
+    #noto-fonts-emoji
+    #iberation_ttf
+    # this causes emacs to crash???
+#    fira-code
+    #fira-code-symbols
+    #mplus-outline-fonts
+    #dina-font
+    #proggyfonts
   ];
+  #fonts.fontconfig.defaultFonts = {
+  #  monospace = [ "JetBrains Mono" ];
+  #  sansSerif = [ "Noto Sans" ];
+  #  serif = [ "noto Serif" ];
+  #};
 }
--- nixos/tools.nix
+++ nixos/tools.nix
@@ -9,5 +9,31 @@
     meson
     dmd
     python3
+    sakura
   ];
+
+  # Terminal Emulators
+  #
+  # gnome-terminal just doesn't work anymore on nixos 21.05
+  # Even after adding "programs.gnome-terminal.enable = true;" (and rebooting), gnome-terminal still fails with
+  #     Error constructing proxy for org.gnome.Terminal:/org/gnome/Terminal/Factory0: Error calling StartServiceByName for org.gnome.Terminal: Timeout was reached
+  #
+  # I tried all the terminal-emulators and I've included the ones that worked over X:
+  #
+  # sakura (seems fast, written in a single C source file)
+  # lilyterm (doesn't seem to handle font correctly, written in C)
+  # st (small font)
+  # lxterminal
+  # roxterm
+  # stupidterm
+  # terminator
+  # termite
+  # tilix
+  # kgx (nice default colors tho)
+  # xterm (small font)
+  #
+  # Tier 3
+  # mlterm (took a minute to exit?)
+  # germinal (requires tmux I think)
+  # hyper (nice font, can't move window)
 }
