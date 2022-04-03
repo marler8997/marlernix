3d124ed2edd0116e5281723226393ce663842925
Jonathan Marler <johnnymarler@gmail.com>
2022-04-03 16:15:40 -0600

    add config for gnome systems

---
+++ nixos/gnome.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # TODO: can I set all this based on whether gnome is enabled?
+  programs.dconf.enable = true;
+  environment.systemPackages = [
+    pkgs.gnome3.adwaita-icon-theme
+    pkgs.gnomeExtensions.appindicator
+  ];
+  services.udev.packages = [
+    pkgs.gnome3.gnome-settings-daemon
+  ];
+  environment.gnome.excludePackages = [
+    pkgs.gnome.cheese
+    pkgs.gnome-photos
+    pkgs.gnome.gnome-music
+    pkgs.gnome.gnome-terminal
+    pkgs.gnome.gedit
+    # install chrome instead with: NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixos.google-chrome
+    # pkgs.epiphany
+    pkgs.evince
+    pkgs.gnome.gnome-characters
+    pkgs.gnome.totem
+    pkgs.gnome.tali
+    pkgs.gnome.iagno
+    pkgs.gnome.hitori
+    pkgs.gnome.atomix
+    pkgs.gnome-tour
+    pkgs.gnome.geary
+  ];
+}
